24dc058cd70764ad300d29eb945740e56744a360
Daniel Shiffman <daniel@shiffman.net>
2017-04-09 10:44:02 -0400

    simple flask example

---
+++ week4-neural-networks/python/index.html
@@ -0,0 +1,1 @@
+Testing one two three
--- week4-neural-networks/python/server.py
+++ week4-neural-networks/python/server.py
@@ -1,23 +1,32 @@
-# Import Flask:
-from flask import Flask, jsonify, request
+#!/usr/bin/python
 
-# Initialize Flask app:
+from flask import Flask, jsonify, request, redirect, url_for, send_from_directory
+
+# Setup Flask app.
 app = Flask(__name__)
+app.debug = True
 
-# Define default route:
+# Routes
 @app.route('/')
-@app.route('/index')
-def index():
-    return jsonify('hello')
+def root():
+  return app.send_static_file('index.html')
+
+# All static files
+@app.route('/<path:path>')
+def static_proxy(path):
+  # send_static_file will guess the correct MIME type
+  return app.send_static_file(path)
+
+## Also now a get request
 
 @app.route('/test')
 def test():
     name = request.args.get('name')
     if name is None:
-        return 'no name';
+        return jsonify(status='no name');
     else:
-        return 'name: ' + name;
-
+        return jsonify(status='name',
+                   name=name)
 # Run app:
 if __name__ == '__main__':
-    app.run( host='0.0.0.0', port=8080, debug=False )
+    app.run( host='0.0.0.0', port=8080, debug=True )
