0ba114befeb29297013b6b73f57e259e505c7bd8
Daniel Shiffman <daniel@shiffman.net>
2017-04-15 16:10:06 -0400

    resnet 50 example

--- week5-cnn-rnn-tensorflow/03_resnet50_keras/static/sketch.js
+++ week5-cnn-rnn-tensorflow/03_resnet50_keras/static/sketch.js
@@ -1,63 +1,69 @@
-var base64;
-var submit;
+var elephant;
 var resultP;
 
-var next = false;
-var drawing = false;
+function preload() {
+  elephant = loadImage('data/elephant.jpg');
+}
 
 function setup() {
-  createCanvas(200, 200);
-  resultP = createP(' ');
-  submit = createButton('classify');
-  submit.mousePressed(classify);
-  background(0);
+  var c = createCanvas(200, 200);
+  resultP = createP('');
+  image(elephant, 0, 0, width, height);
+  classify(elephant);
+
+  // Add an event for when a file is dropped onto the canvas
+  c.drop(gotFile);
 }
 
-function mousePressed() {
-  drawing = true;
-  if (next) {
-    background(0);
-    next = false;
+function classify(img) {
+
+  var base64;
+  if (typeof(img) == 'string') {
+    base64 = img;
+  } else if (img instanceof p5.Image) {
+    base64 = img.canvas.toDataURL();
+  } else {
+    return;
   }
-}
 
-function mouseReleased() {
-  drawing = false;
-}
-
-function draw() {
-  if (drawing) {
-    stroke(255);
-    strokeWeight(16);
-    line(pmouseX, pmouseY, mouseX, mouseY);
-  }
-}
-
-function classify() {
-  var img = get();
-  var base64 = img.canvas.toDataURL();
-  var cleaned = base64.replace('data:image/png;base64,', '');
+  var cleaned = base64.replace(/data:image\/(png|jpeg|jpg|gif);base64,/, '');
   var data = {
     img: cleaned
   }
-
   httpPost('/upload', data, success, error);
-
   function success(reply) {
     var result = JSON.parse(reply);
     console.log(result);
-    if (result.number != undefined) {
-      var number = result.number;
-      var confidence = result.prediction[number];
-      resultP.html(number + '<br/>' + 'confidence: ' + nf(100 * confidence, 2, 1) + '%');
-    } else {
-      resultP.html('error');
+    var report = '';
+    for (var i = 0; i < result.prediction.length; i++) {
+      var term = result.prediction[i].term;
+      var confidence = result.prediction[i].score;
+      report += term + ' (confidence: ' + nf(100 * confidence, 2, 1) + '%) <br/>';
     }
-    next = true;
+    resultP.html(report);
   }
 
   function error(reply) {
     console.log(reply);
     resultP.html('error');
+  }
+}
+
+function gotFile(file) {
+  // If it's an image file
+  if (file.type === 'image') {
+    var data = file.data;
+    classify(data);
+    // Create an image DOM element but don't show it
+    img = createImg(data, ready);
+
+    function ready() {
+      img.hide();
+      background(0);
+      image(img, 0, 0, width, height);
+    }
+    // Draw the image onto the canvas
+  } else {
+    console.log('Not an image file!');
   }
 }
