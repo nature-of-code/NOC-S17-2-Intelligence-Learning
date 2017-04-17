472b5c22820178d6ac2effcece8e8304a5a88c39
Daniel Shiffman <daniel@shiffman.net>
2017-04-17 19:03:47 -0400

    finish commenting and cleanup

--- week5-cnn-rnn-tensorflow/04b_rnn_flask_js/server.py
+++ week5-cnn-rnn-tensorflow/04b_rnn_flask_js/server.py
@@ -1,18 +1,25 @@
-#!/usr/bin/python
+# Daniel Shiffman
+# Nature of Code: Intelligence and Learning
+# https://github.com/shiffman/NOC-S17-2-Intelligence-Learning
 
-# import flask
-from flask import Flask, jsonify, request, redirect, url_for, send_from_directory
+# import everything we need
+from flask import Flask, jsonify, request
 
+# ML stuff
 import keras
 from keras.models import load_model
 import numpy as np
 
 # Setup Flask app.
 app = Flask(__name__)
+# Extra debugging
 app.debug = True
 
+
+# helper function to sample an index from a probability array
+# This comes directly from
+# https://github.com/fchollet/keras/blob/master/examples/lstm_text_generation.py
 def sample(preds, temperature=1.0):
-    # helper function to sample an index from a probability array
     preds = np.asarray(preds).astype('float64')
     preds = np.log(preds) / temperature
     exp_preds = np.exp(preds)
@@ -20,49 +27,74 @@ def sample(preds, temperature=1.0):
     probas = np.random.multinomial(1, preds, 1)
     return np.argmax(probas)
 
+# Load in the model
 model = load_model('model.h5')
 
+# Same maxlen as in the model
 maxlen = 40
-text = open('hamlet.txt').read().lower()  # read the file and convert to lowercase
+
+# We have to build the same dictionaries here from the source text
+# We could instead save and load these instead?
+# read the file and convert to lowercase (must match how we did it during training)
+text = open('data/hamlet.txt').read().lower()
+# Get a list of all the unique characters
 chars = sorted(list(set(text)))
-# what position does each character exist at in the prev list
+# Two dictionaries
+# Lookup a character by index
+# Lookup an index by character
 char_indices = dict((c,i) for i, c in enumerate(chars))
 indices_char = dict((i,c) for i,c in enumerate(chars))
 
 
 # Routes
+# This is root path, use index.html in "static" folder
 @app.route('/')
 def root():
   return app.send_static_file('index.html')
 
-# All static files
+# This is a nice way to just serve everything in the "static" folder
 @app.route('/<path:path>')
 def static_proxy(path):
   # send_static_file will guess the correct MIME type
   return app.send_static_file(path)
 
-## Also now a post request to receive a file
+# This is the new piece
+# Here we receive the "seed" text from p5 via a "POST" request
+# and send back the generated text
 @app.route('/upload', methods=['POST'])
 def upload():
+    # Grab the seed sentence
+    # Should check to make sure it is maxlen chars
     sentence = request.form['seed']
-    print(sentence)
-    # sentence = 'ince of denmark hamlet prince of denmark'
-    diversity = float(request.form['temperature'])
+    # Grab diversity
+    temperature = float(request.form['temperature'])
+    # Grab length (how many chars to generate)
     length = int(request.form['length'])
+    # Start with empty generated string
     generated = ''
-    # generated += sentence
+
     for i in range(length):
+        # First vectorize the text we are feeding in
         x = np.zeros((1, maxlen, len(chars)))
         for t, char in enumerate(sentence):
             x[0, t, char_indices[char]] = 1.
+        # Then get a probability map of possible predictions of the next char
         preds = model.predict(x, verbose=0)[0]
+        # Pick one
         next_index = sample(preds, diversity)
+        # What character is it?
         next_char = indices_char[next_index]
+        # Add it
         generated += next_char
+        # Now feed in the previous sentence minus the first char plus the next char
         sentence = sentence[1:] + next_char
+    # send back the generated sentence
     return jsonify(status='success',sentence=generated);
 
 # Run app:
 if __name__ == '__main__':
-#    app.run( host='0.0.0.0', port=8080, debug=True )
+    # Localhost and port 8080
     app.run( host='0.0.0.0', port=8080, debug=False )
+    # If you enable debugging, you'll get more info
+    # server will restart automatically when code changes, etc.
+    # app.run( host='0.0.0.0', port=8080, debug=True )
--- week5-cnn-rnn-tensorflow/04b_rnn_flask_js/static/sketch.js
+++ week5-cnn-rnn-tensorflow/04b_rnn_flask_js/static/sketch.js
@@ -1,19 +1,30 @@
+// Daniel Shiffman
+// Nature of Code: Intelligence and Learning
+// https://github.com/shiffman/NOC-S17-2-Intelligence-Learning
+
+// Seed text from user
 var textInput;
+// Temperature slider
+var tempSlider;
+// Length to generate slider
+var lengthSlider;
+
+// Maxlen paramter from the model
 var maxlen = 40;
 
-var suggestionP;
-
+// Variable to stop the sketch from querying the server too often
 var waiting = false;
 
-var slider;
-var lengthInput;
 
 function setup() {
   noCanvas();
+
+  // Grab the DOM elements
   textInput = select('#textInput');
   lengthSlider = select('#lenSlider');
   tempSlider = select('#tempSlider');
 
+  // Run generate anytime something changes
   textInput.input(generate);
   lengthSlider.input(generate);
   tempSlider.input(generate);
@@ -22,12 +33,18 @@ function setup() {
 
 function generate() {
 
+  // Update the length and temperature span elements
   select('#length').html(lengthSlider.value())
   select('#temperature').html(tempSlider.value())
 
+  // Grab the original text
   var original = textInput.value();
+  // Make it to lower case
   var txt = original.toLowerCase();
 
+  // This is some goofy parsing to make sure the seed text is
+  // always 40 characters. This should probably be done
+  // one the python side.
   while (txt.length < maxlen) {
     txt += original.toLowerCase();
   }
@@ -35,26 +52,33 @@ function generate() {
     txt = txt.substring(txt.length - maxlen, txt.length);
   }
 
+  // Here is the data to post
   var data = {
     seed: txt,
-    temperature: 0.5,
+    temperature: tempSlider.value(),
     length: lengthSlider.value()
   }
 
+  // If we aren't waiting for previous results, go ahead and post
   if (!waiting) {
+    // Now we are waiting
     waiting = true;
-    console.log('seed: ' + txt);
+    console.log('Posting seed: ' + txt);
+    // Post to the server
     httpPost('/upload', data, success, error);
   }
 
+  // WE got a reply
   function success(reply) {
     var result = JSON.parse(reply);
-    // console.log(result);
+    // Update the DOM elements
     select('#original').html(original);
     select('#prediction').html(result.sentence);
+    // We're not waiting anymore
     waiting = false;
   }
 
+  // Error
   function error(reply) {
     console.log(reply);
   }
