bfecd9d88b64ad3c7012c7ad7618c49368e07f4a
Daniel Shiffman <daniel@shiffman.net>
2017-04-17 13:35:26 -0400

    code comments and cleanup

--- week5-cnn-rnn-tensorflow/00_p5_convolution/convolution1/sketch.js
+++ week5-cnn-rnn-tensorflow/00_p5_convolution/convolution1/sketch.js
@@ -1,34 +1,52 @@
+// Daniel Shiffman
+// Nature of Code: Intelligence and Learning
+// https://github.com/shiffman/NOC-S17-2-Intelligence-Learning
+
+// An example to visualize convolution steps in CNN
+
+// Source image
 var img;
+// Processed pixels
 var dest;
+
+// Size of processed pixels
 var w = 80;
 
+// Where to process the pixels
 var xstart = 60;
 var ystart = 90;
 
-// The convolution kernel for a "sharpen" effect stored as a 3 x 3 two-dimensional array.
+// The convolution kernel for a "sharpen" effect
+// stored as a 3 x 3 two-dimensional array.
 var kernel = [
   [-1, -1, -1],
   [-1, 9, -1],
   [-1, -1, -1]
 ];
 
+// checkboxes for visualizing relu and/or maxpooling
 var relu;
 var maxpooling;
 
+// Load an image
 function preload() {
   img = loadImage("data/kitten.jpg");
 }
 
 function setup() {
   createCanvas(256, 256);
+  // turn off HPDI displays (like retina)
   pixelDensity(1);
+  // Create a blank image to write processed pixels to
   dest = createImage(w, w);
+  // Checkboxes
   relu = createCheckbox('relu');
   maxpooling = createCheckbox('maxpooling');
-
+  // Button to randomize weights
   var button = createButton('randomize kernel');
   button.mousePressed(randomizekernel);
 
+  // Pick random weights for the pixels
   function randomizekernel() {
     for (var i = 0; i < 3; i++) {
       for (var j = 0; j < 3; j++) {
@@ -44,100 +62,108 @@ function draw() {
   // so let's set the whole image as the background first
   image(img, 0, 0);
 
-  // var speed = 2;
-  // xstart += speed;
-  // if (xstart > img.width - w) {
-  //   xstart = 0;
-  //   ystart += speed;
-  //   if (ystart > img.height - w) {
-  //     ystart = 0;
-  //   }
-  // }
-
-  // In this example we are only processing a section of the image-an 80 x 80 rectangle around the mouse location.
+  // In this example we are only processing a section of the image
+  // an 80 x 80 rectangle
   var xend = xstart + w;
   var yend = ystart + w;
   var kernelsize = 3;
 
+  // Load all the pixels
   dest.loadPixels();
   img.loadPixels();
-  // Begin our loop for every pixel
 
+  // Begin our loop for every pixel
   for (var x = 0; x < dest.width; x++) {
     for (var y = 0; y < dest.height; y++) {
       // Each pixel location (x,y) gets passed into a function called convolution()
       // The convolution() function returns a new color to be displayed.
-      var result = convolution(x + xstart, y + ystart, kernel, kernelsize, img);
-      var loc = (x + y * dest.width) * 4;
-      dest.pixels[loc + 0] = result[0];
-      dest.pixels[loc + 1] = result[1];
-      dest.pixels[loc + 2] = result[2];
+      var result = convolution(img, x + xstart, y + ystart, kernel, kernelsize);
+      var index = (x + y * dest.width) * 4;
+      dest.pixels[index + 0] = result[0];
+      dest.pixels[index + 1] = result[1];
+      dest.pixels[index + 2] = result[2];
       dest.pixels[loc + 3] = 255;
     }
   }
   dest.updatePixels();
 
-
+  // Draw the convolved feature map
   image(dest, xstart, ystart);
   if (maxpooling.checked()) {
     maxpool(dest, 5, xstart, ystart);
   }
+
+  // Draw a rectangle for visual clarity
   stroke(0);
   noFill();
   rectMode(CORNERS);
   rect(xstart, ystart, xend, yend);
-
 }
 
-function convolution(x, y, kernel, kernelsize, img) {
-  var rtotal = 0.0;
-  var gtotal = 0.0;
-  var btotal = 0.0;
+function convolution(img, x, y, kernel, kernelsize) {
+
+  // Going to sum the RGB values of all the pixels
+  var rsum = 0.0;
+  var gsum = 0.0;
+  var bsum = 0.0;
+
+  // Offset around the center pixel
   var offset = floor(kernelsize / 2);
 
   // Loop through convolution kernel
   for (var i = 0; i < kernelsize; i++) {
     for (var j = 0; j < kernelsize; j++) {
+
       // What pixel are we testing
-      var xloc = x + i - offset;
-      var yloc = y + j - offset;
-      var loc = (xloc + img.width * yloc) * 4;
+      var xpos = x + i - offset;
+      var ypos = y + j - offset;
+      // Find the 1D location in the array
+      var index = (xpos + img.width * ypos) * 4;
 
       // Make sure we haven't walked off the edge of the pixel array
       // It is often good when looking at neighboring pixels to make sure we have not gone off the edge of the pixel array by accident.
-      loc = constrain(loc, 0, img.pixels.length - 1);
+      index = constrain(loc, 0, img.pixels.length - 1);
+
       // Calculate the convolution
-      // We sum all the neighboring pixels multiplied by the values in the convolution kernel.
-      rtotal += img.pixels[loc + 0] * kernel[i][j];
-      gtotal += img.pixels[loc + 1] * kernel[i][j];
-      btotal += img.pixels[loc + 2] * kernel[i][j];
+      // We sum all the neighboring pixels
+      // multiplied by the weights in the convolution kernel.
+      rsum += img.pixels[loc + 0] * kernel[i][j];
+      gsum += img.pixels[loc + 1] * kernel[i][j];
+      bsum += img.pixels[loc + 2] * kernel[i][j];
     }
   }
 
-  // Make sure RGB is within range
+  // To emphasize ReLU I'm mapping negative pixels to a positive range
+  rsum = map(rsum, -255, 255, 0, 255);
+  gsum = map(gsum, -255, 255, 0, 255);
+  bsum = map(bsum, -255, 255, 0, 255);
 
-  rtotal = map(rtotal, -255, 255, 0, 255);
-  gtotal = map(gtotal, -255, 255, 0, 255);
-  btotal = map(btotal, -255, 255, 0, 255);
+  // And then, capping the range and mapping it back to demonstrate ReLU
+  // Better way to do this?
   if (relu.checked()) {
-    rtotal = constrain(rtotal, 127, 255);
-    gtotal = constrain(gtotal, 127, 255);
-    btotal = constrain(btotal, 127, 255);
-    rtotal = map(rtotal, 127, 255, 0, 255);
-    gtotal = map(gtotal, 127, 255, 0, 255);
-    btotal = map(btotal, 127, 255, 0, 255);
+    rsum = constrain(rsum, 127, 255);
+    gsum = constrain(gsum, 127, 255);
+    bsum = constrain(bsum, 127, 255);
+    rsum = map(rsum, 127, 255, 0, 255);
+    gsum = map(gsum, 127, 255, 0, 255);
+    bsum = map(bsum, 127, 255, 0, 255);
   }
 
   // Return an array with the three color values
-  return [rtotal, gtotal, btotal];
+  return [rsum, gsum, bsum];
 }
 
 
+// This maxpooling function will iterate over all the
+// "pooled" areas and draw a rectangle showing the
+// brightest pixel
 function maxpool(img, skip, xoff, yoff) {
-  //img.loadPixels();
+  // Check all the pixels
   for (var x = 0; x < img.width; x += skip) {
     for (var y = 0; y < img.height; y += skip) {
+      // Find the brightest pixel
       var brightest = findMax(img, x, y, skip);
+      // Draw the rectangle
       fill(brightest[0], brightest[1], brightest[2]);
       noStroke();
       rectMode(CORNER);
@@ -146,21 +172,28 @@ function maxpool(img, skip, xoff, yoff) {
   }
 }
 
+// This function finds the brightest pixel in a smaller area
 function findMax(img, xstart, ystart, skip) {
+  // Brightest so far
   var record = 0;
   var brightest = [0, 0, 0];
   for (var x = 0; x < skip; x++) {
     for (var y = 0; y < skip; y++) {
+      // Find the 1D location in the array
       var index = ((x + xstart) + (y + ystart) * img.width) * 4;
+      // Look at RGB
       var r = img.pixels[index + 0];
       var g = img.pixels[index + 1];
       var b = img.pixels[index + 2];
+      // Add it up
       var sum = r + g + b;
+      // Is this the new brightest pixel?
       if (sum > record) {
         record = sum;
         brightest = [r, g, b];
       }
     }
   }
+  // Return the result
   return brightest;
 }
--- week5-cnn-rnn-tensorflow/01_simple_flask_p5/static/sketch.js
+++ week5-cnn-rnn-tensorflow/01_simple_flask_p5/static/sketch.js
@@ -1,22 +1,33 @@
+// Daniel Shiffman
+// Nature of Code: Intelligence and Learning
+// https://github.com/shiffman/NOC-S17-2-Intelligence-Learning
 
+// Text input
 var input;
+// Submit button
 var submitButton;
 
 function setup() {
-  createCanvas(200,200);
+  createCanvas(200, 200);
   background(51);
+  // Text input
   input = createInput('enter your name');
+  // Submit button
   var submitButton = createButton('submit');
   submitButton.mousePressed(submit);
 
+  // Send data to python Flask server
   function submit() {
     var name = input.value();
     loadJSON('/test?name=' + name, gotData);
   }
 }
 
+// Reply back from flask server
 function gotData(data) {
   console.log(data);
+
+  // Draw the name when it comes back in the canvas
   var name = data.name;
   var x = random(width);
   var y = random(height);
