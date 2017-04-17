c83f7c404a5fe3bdf07bbec93da93666ca4a951d
Daniel Shiffman <daniel@shiffman.net>
2017-04-17 15:23:20 -0400

    comments and cleanup for cnn example

--- week5-cnn-rnn-tensorflow/02b_cnn_flask_p5/server.py
+++ week5-cnn-rnn-tensorflow/02b_cnn_flask_p5/server.py
@@ -1,48 +1,76 @@
 #!/usr/bin/python
 
-# import flask
-from flask import Flask, jsonify, request, redirect, url_for, send_from_directory
+# import everything we need
+from flask import Flask, jsonify, request
 
+# Some file reading and image stuff
 from io import BytesIO
 from PIL import Image
-import numpy as np
-import matplotlib.pyplot as plt
 from scipy.misc import imresize
 import base64
+import matplotlib.pyplot as plt
+
+# ML stuff
+import numpy as np
 import keras
 from keras.models import load_model
 
 # Setup Flask app.
 app = Flask(__name__)
+# Extra debugging
 app.debug = True
 
+# Load in the model
 model = load_model('model.h5')
 
 # Routes
+# This is root path, use index.html in "static" folder
 @app.route('/')
 def root():
   return app.send_static_file('index.html')
 
-# All static files
+# This is a nice way to just serve everything in the "static" folder
 @app.route('/<path:path>')
 def static_proxy(path):
   # send_static_file will guess the correct MIME type
   return app.send_static_file(path)
 
-## Also now a post request to receive a file
+# This function converts RGB values to grayscale (1/3rd of each color)
+# This is a somewhat silly way to do this but perhaps it's a useful demonstration
+# of reshaping data
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.333, 0.333, 0.333])
+
+# This is the new piece
+# Here we receive a "base64" encoded image from p5
+# via a "POST" request
 @app.route('/upload', methods=['POST'])
 def upload():
+    # Get the image data from the POST
     data = request.form['img']
+    # This converts it to an image
     img = Image.open(BytesIO(base64.b64decode(data)))
+    # Now resize it to 28x28x3
     img = imresize(img, (28,28,3))
-    gray = four2one(img)
+    # Run the conversion
+    gray = rgb2gray(img)
+    # map values to between 0 and 1
     gray /= 255
+    # Reshap the data
+    # This is one 28x28 image with one channel (gray)
+    # 1 x 28 x 28 x 1
     inputs = gray.reshape(1, 28, 28, 1)
+    # Probabilities
     prediction = model.predict(inputs);
+    # What digit is it? (we could just pull the max prob from above, but showing both options)
     label = model.predict_classes(inputs)
+    # Send everything back as JSON
     return jsonify(status='got image',number=label.tolist()[0],prediction=prediction.tolist()[0]);
 
 # Run app:
 if __name__ == '__main__':
-#    app.run( host='0.0.0.0', port=8080, debug=True )
+    # Localhost and port 8080
     app.run( host='0.0.0.0', port=8080, debug=False )
+    # If you enable debugging, you'll get more info
+    # server will restart automatically when code changes, etc.
+    # app.run( host='0.0.0.0', port=8080, debug=True )
--- week5-cnn-rnn-tensorflow/02b_cnn_flask_p5/static/sketch.js
+++ week5-cnn-rnn-tensorflow/02b_cnn_flask_p5/static/sketch.js
@@ -1,31 +1,44 @@
-var base64;
+// Daniel Shiffman
+// Nature of Code: Intelligence and Learning
+// https://github.com/shiffman/NOC-S17-2-Intelligence-Learning
+
+// Submit button
 var submit;
+// Show results
 var resultP;
 
 var next = false;
 var drawing = false;
 
 function setup() {
-  createCanvas(200, 200);
+  // Create DOM elements
+  var canvas = createCanvas(200, 200);
+  canvas.mousePressed(startDrawing);
+  canvas.mouseReleased(stopDrawing);
   resultP = createP(' ');
   submit = createButton('classify');
+  // When the button is pressed classify!
   submit.mousePressed(classify);
   background(0);
 }
 
-function mousePressed() {
+// Turn drawing on
+function startDrawing() {
   drawing = true;
   if (next) {
+    // Clear the background
     background(0);
     next = false;
   }
 }
 
-function mouseReleased() {
+// Turn drawing off when you release
+function stopDrawing() {
   drawing = false;
 }
 
 function draw() {
+  // If you are drawing
   if (drawing) {
     stroke(255);
     strokeWeight(16);
@@ -33,22 +46,34 @@ function draw() {
   }
 }
 
+// Run the classification
 function classify() {
+  // Get all the pixels!
   var img = get();
+  // Turn it to a base64 encoded string
   var base64 = img.canvas.toDataURL();
+  // Get rid of the header stuff (should maybe do this in flask)
   var cleaned = base64.replace('data:image/png;base64,', '');
+  // Make an object to post
   var data = {
     img: cleaned
   }
 
+  // Post the data!
   httpPost('/upload', data, success, error);
 
+  // Here is where we get a reply
   function success(reply) {
+    // Parse the reply
     var result = JSON.parse(reply);
     console.log(result);
+
+    // As long as we got something
     if (result.number != undefined) {
+      // Look at number and probability (confidence)
       var number = result.number;
       var confidence = result.prediction[number];
+      // Display the results in the paragraph element
       resultP.html(number + '<br/>' + 'confidence: ' + nf(100 * confidence, 2, 1) + '%');
     } else {
       resultP.html('error');
